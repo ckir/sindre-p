41370a6ca9ba6d88e68d50fa785bd5d6e331b3db
Costas Kirgoussios <ckir1508@hotmail.com>
2023-03-29 07:04:28 +0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,104 @@
-export default {
-    all: require('p-all'),
-    any: require('p-any'),
-    break: require('p-break'),
-    cancelable: require('p-cancelable'),
-    catchIf: require('p-catch-if'),
-    debounce: require('p-debounce'),
-    defer: require('p-defer'),
-    delay: require('delay'),
-    doWhilst: require('p-do-whilst'),
-    eachSeries: require('p-each-series'),
-    event: require('p-event'),
-    every: require('p-every'),
-    filter: require('p-filter'),
-    forever: require('p-forever'),
-    hardRejection: require('hard-rejection'),
-    if: require('p-if'),
-    immediate: require('p-immediate'),
-    isPromise: require('p-is-promise'),
-    lazy: require('p-lazy'),
-    limit: require('p-limit'),
-    locate: require('p-locate'),
-    log: require('p-log'),
-    loudRejection: require('loud-rejection'),
-    map: require('p-map'),
-    mapSeries: require('p-map-series'),
-    memoize: require('p-memoize'),
-    minDelay: require('p-min-delay'),
-    one: require('p-one'),
-    pify: require('pify'),
-    pipe: require('p-pipe'),
-    progress: require('p-progress'),
-    props: require('p-props'),
-    queue: require('p-queue'),
-    race: require('p-race'),
-    reduce: require('p-reduce'),
-    reflect: require('p-reflect'),
-    retry: require('p-retry'),
-    series: require('p-series'),
-    settle: require('p-settle'),
-    some: require('p-some'),
-    tap: require('p-tap'),
-    throttle: require('p-throttle'),
-    time: require('p-time'),
-    timeout: require('p-timeout'),
-    times: require('p-times'),
-    try: require('p-try'),
-    waitFor: require('p-wait-for'),
-    waterfall: require('p-waterfall'),
-    whilst: require('p-whilst')
-  }
+import pAll from 'p-all';
+import pAny from 'p-any';
+// import pBreak from 'p-break';
+import PCancelable from 'p-cancelable';
+import pCatchIf from 'p-catch-if';
+import pDebounce from 'p-debounce';
+import pDefer from 'p-defer';
+import delay from 'delay';
+import pDoWhilst from 'p-do-whilst';
+import pEachSeries from 'p-each-series';
+import { pEvent } from 'p-event';
+import pEvery from 'p-every';
+import pFilter from 'p-filter';
+import pForever from 'p-forever';
+import hardRejection from 'hard-rejection';
+import pIf from 'p-if';
+import pImmediate from 'p-immediate';
+import isPromise from 'p-is-promise';
+import PLazy from 'p-lazy';
+import pLimit from 'p-limit';
+import pLocate from 'p-locate';
+import pLog from 'p-log';
+import loudRejection from 'loud-rejection';
+import pMap from 'p-map';
+import pMapSeries from 'p-map-series';
+import pMemoize from 'p-memoize';
+import pMinDelay from 'p-min-delay';
+import pOne from 'p-one';
+import pify from 'pify';
+import pPipe from 'p-pipe';
+import pProgress from 'p-progress';
+import pProps from 'p-props';
+import PQueue from 'p-queue';
+import pRace from 'p-race';
+import pReduce from 'p-reduce';
+import pReflect from 'p-reflect';
+import pRetry from 'p-retry';
+import pSeries from 'p-series';
+import pSettle from 'p-settle';
+import pSome from 'p-some';
+import pTap from 'p-tap';
+import pThrottle from 'p-throttle';
+import pTime from 'p-time';
+import pTimeout from 'p-timeout';
+import pTimes from 'p-times';
+import pTry from 'p-try';
+import pWaitFor from 'p-wait-for';
+import pWaterfall from 'p-waterfall';
+import pWhilst from 'p-whilst';
+
+const p:object  = {
+    all: pAll,
+    any: pAny,
+    // break: pBreak,
+    cancelable: PCancelable,
+    catchIf: pCatchIf,
+    debounce: pDebounce,
+    defer: pDefer,
+    delay: delay,
+    doWhilst: pDoWhilst,
+    eachSeries: pEachSeries,
+    event: pEvent,
+    every: pEvery,
+    filter: pFilter,
+    forever: pForever,
+    hardRejection: hardRejection,
+    if: pIf,
+    immediate: pImmediate,
+    isPromise: isPromise,
+    lazy: PLazy,
+    limit: pLimit,
+    locate: pLocate,
+    log: pLog,
+    loudRejection: loudRejection,
+    map: pMap,
+    mapSeries: pMapSeries,
+    memoize: pMemoize,
+    minDelay: pMinDelay,
+    one: pOne,
+    pify: pify,
+    pipe: pPipe,
+    progress: pProgress,
+    props: pProps,
+    queue: PQueue,
+    race: pRace,
+    reduce: pReduce,
+    reflect: pReflect,
+    retry: pRetry,
+    series: pSeries,
+    settle: pSettle,
+    some: pSome,
+    tap: pTap,
+    throttle: pThrottle,
+    time: pTime,
+    timeout: pTimeout,
+    times: pTimes,
+    try: pTry,
+    waitFor: pWaitFor,
+    waterfall: pWaterfall,
+    whilst: pWhilst
+};
+
+export default p
+
